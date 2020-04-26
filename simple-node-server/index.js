8cafaf710cd2b579b740786b3fee8edc17963521
George Montgomery <montgomery.wg@gmail.com>
2020-04-26 14:30:25 +0100

    updated simple-node-server

--- simple-node-server/index.js
+++ simple-node-server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 4000;
 require("./routes/routes")(app);
 
-app.listen(process.env.PORT || 4000);
+app.listen(PORT, () => {
+  console.log("Server running on port %d", PORT);
+});
--- simple-node-server/routes/routes.js
+++ simple-node-server/routes/routes.js
@@ -1,5 +1,15 @@
 module.exports = (app) => {
+  // const pino = require("pino");
+  // const expressPino = require("express-pino-logger");
+  // const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+  // const expressLogger = expressPino({ logger });
+  // app.use(expressLogger);
+
+  const loc = process.env.LOCATION || "Local";
+  const s = `hello from ${loc}, how are you doing?`;
   app.get("/", function (req, res) {
-    res.send("Hello World");
+    // logger.debug("Calling res.send");
+    res.send(s);
+    // console.log(req);
   });
 };
